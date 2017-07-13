d02cb003e840cb7546435536056c7a46b7379cae
Demur Rumed <gunkmute@gmail.com>
2017-07-13 03:05:28 +0000

    Not-Even-MVP MVP
    Currently only returns a result for an exact match
    Was going OOM when attempting to read CSV into a Vec<StringRecord>
    Need to bring in serde
    Need to figure out how to unit test Iron
    Should probably not require a recompile for updating data or html

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,91 @@
 extern crate iron;
 extern crate router;
+extern crate urlencoded;
+
+extern crate csv;
+extern crate fnv;
+#[macro_use] extern crate lazy_static;
 
 use std::env;
-use iron::{Iron, Request, Response, IronResult};
+use fnv::FnvHashMap;
+use iron::{Iron, IronResult, Plugin, Request, Response, status, headers};
+use iron::mime::{Mime, TopLevel, SubLevel};
 use router::Router;
-use iron::status;
+use urlencoded::UrlEncodedQuery;
 
-fn hello(_: &mut Request) -> IronResult<Response> {
-    let resp = Response::with((status::Ok, "Hello world!"));
+static INDEXHTML: &'static str = include_str!("index.html");
+static DATACSV: &'static str = include_str!("../data/cities_canada-usa.tsv");
+
+struct LocationRecord {
+    id: u64,
+    name: String,
+    ascii: String,
+    alt_name: String,
+    lat: f64,
+    long: f64,
+    feat_class: String,
+    feat_code: String,
+    country: String,
+    cc2: String,
+    admin1: String,
+    admin2: String,
+    admin3: String,
+    admin4: String,
+    population: u64,
+    elevation: u64,
+    dem: u64,
+    tz: String,
+    modified_at: String,
+}
+
+struct LatLong(f64, f64);
+
+lazy_static!{
+    static ref GEODATA: FnvHashMap<String, LatLong> = {
+        let mut map: FnvHashMap<String, LatLong> = FnvHashMap::default();
+        let mut rdr = csv::ReaderBuilder::new()
+            .has_headers(true)
+            .delimiter(b'\t')
+            .from_reader(DATACSV.as_bytes());
+        for record in rdr.records().filter_map(|x| x.ok()) {
+            map.insert(String::from(&record[1]), LatLong(record[4].parse().unwrap(), record[5].parse().unwrap()));
+        }
+        map
+    };
+}
+
+fn index(_: &mut Request) -> IronResult<Response> {
+    let mut resp = Response::with((status::Ok, INDEXHTML));
+    resp.headers.set(headers::ContentType(Mime(TopLevel::Text, SubLevel::Html, Vec::new())));
     Ok(resp)
 }
 
-fn hello_name(req: &mut Request) -> IronResult<Response> {
-    let params = req.extensions.get::<Router>().unwrap();
-    let name = params.find("name").unwrap();
-    let resp = Response::with((status::Ok, format!("{}", name)));
+fn suggestions(req: &mut Request) -> IronResult<Response> {
+    let resptext = match req.get_ref::<UrlEncodedQuery>() {
+        Ok(ref gets) => {
+            let gqf = gets.get("q").and_then(|gq| gq.first());
+            if let Some(exact) = gqf.and_then(|gqf| GEODATA.get(gqf)) {
+                format!("{{suggestions:[{{latitude:\"{}\",longitude:\"{}\"}}]}}", exact.0, exact.1)
+            } else {
+                format!("GET {}: {:?}", get_server_port(), gets)
+            }
+        },
+        Err(ref e) => format!("Error: {:?}", e),
+    };
+    let mut resp = Response::with((status::Ok, resptext));
+    resp.headers.set(headers::ContentType(Mime(TopLevel::Application, SubLevel::Json, Vec::new())));
     Ok(resp)
 }
 
 fn get_server_port() -> u16 {
     let port_str = env::var("PORT").unwrap_or(String::new());
-    port_str.parse().unwrap_or(8080)
+    port_str.parse().unwrap_or(80)
 }
 
 fn main() {
     let mut router: Router = Router::new();
-    router.get("/", hello, "index");
-    router.get("/:name", hello_name, "name");
+    router.get("/", index, "index");
+    router.get("/suggestions", suggestions, "suggestions");
 
     Iron::new(router).http(("0.0.0.0", get_server_port())).unwrap();
-    println!("Hello, world!");
 }
